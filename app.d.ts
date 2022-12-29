cb48b870672b15b2f78bc9a59d41463c68a19d4c
Pontus Östlund <ponost@roxen.com>
2022-12-29 07:33:38 +0100

    fix(types): Proper type in sample .d.ts for component
    Cudos to @YummYume for the code in #29

--- app.d.ts
+++ app.d.ts
@@ -1,12 +1,11 @@
-/// <reference types="@sveltejs/kit" />
-
-declare module '*.svg' {
-  const content: ConstructorOfATypedSvelteComponent
-  export default content
-}
-
 declare module '*.svg?component' {
-  const content: ConstructorOfATypedSvelteComponent
+  import type { ComponentType, SvelteComponentTyped } from 'svelte'
+  import type { SVGAttributes } from 'svelte/elements'
+
+  const content: ComponentType<
+    SvelteComponentTyped<SVGAttributes<SVGSVGElement>>
+  >
+
   export default content
 }
 
--- test-app/src/app.d.ts
+++ test-app/src/app.d.ts
@@ -1,7 +1,11 @@
-/// <reference types="@sveltejs/kit" />
-
 declare module '*.svg?component' {
-  const content: ConstructorOfATypedSvelteComponent
+  import type { ComponentType, SvelteComponentTyped } from 'svelte'
+  import type { SVGAttributes } from 'svelte/elements'
+
+  const content: ComponentType<
+    SvelteComponentTyped<SVGAttributes<SVGSVGElement>>
+  >
+
   export default content
 }
 
