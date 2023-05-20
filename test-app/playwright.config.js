0e7e7efa63d84c8bd5c139113a7466039700c380
Pontus Östlund <ponost@roxen.com>
2023-05-20 09:59:31 +0200

    chore: Updated packages in test app

--- test-app/playwright.config.js
+++ test-app/playwright.config.js
@@ -1,5 +1,6 @@
 /** @type {import('@playwright/test').PlaywrightTestConfig} */
 const config = {
+  testMatch: /tests\/.*.js$/,
   webServer: {
     command: 'npm run build && npm run preview',
     port: 4173,
