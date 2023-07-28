7ce6849ca603bfb04d83c02be67c69cbff2ed627
Pontus Östlund <ponost@roxen.com>
2023-07-28 11:07:40 +0200

    chore: Added ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    // sourceType: 'module',
+    ecmaVersion: 2020,
+  },
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+  },
+  rules: {
+    'no-unused-vars': 'off',
+    curly: 'error',
+    yoda: 'error',
+    'default-case': 'error',
+    camelcase: 'off',
+    eqeqeq: ['error', 'always'],
+    'no-case-declarations': 'error',
+    'no-new-wrappers': 'error',
+    'no-return-await': 'error',
+    'no-self-compare': 'error',
+    'no-useless-call': 'error',
+    'no-multi-spaces': 'error',
+    'no-trailing-spaces': 'error',
+    'no-irregular-whitespace': 'error',
+    'max-len': [
+      'error',
+      {
+        code: 80,
+        ignoreUrls: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+        ignoreRegExpLiterals: true,
+      },
+    ],
+  },
+}
