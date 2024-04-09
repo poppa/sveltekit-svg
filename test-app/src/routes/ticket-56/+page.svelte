daae923f7e6ee7fc0a36888f9b02ffc0ab9ae0fa
Pontus Östlund <92401+poppa@users.noreply.github.com>
2024-04-09 14:45:31 +0200

    Added test files for #56

---
+++ test-app/src/routes/ticket-56/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import Sample from '../../assets/ticket-56.svg?component'
+  import sampleRaw from '../../assets/ticket-56.svg?src'
+  import sampleUrl from '../../assets/ticket-56.svg?url'
+</script>
+
+<div>
+  <p>Sample Component</p>
+  <Sample />
+</div>
+
+<div>
+  <p>Sample Raw</p>
+  <!-- eslint-disable-next-line svelte/no-at-html-tags -->
+  {@html sampleRaw}
+</div>
+
+<div>
+  <p>Sample Img</p>
+  <img src={sampleUrl} alt="" />
+</div>
