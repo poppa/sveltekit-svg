658f0bfcad5045ed7c2f4e4674710286fb07653e
Craig Jennings <1683368+craig-jennings@users.noreply.github.com>
2025-08-30 08:53:47 -0500

    feat: update svgo to v4

--- README.md
+++ README.md
@@ -46,9 +46,6 @@ const config = {
         plugins: [
           {
             name: 'preset-default',
-            // by default svgo removes the viewBox which prevents svg icons from scaling
-            // not a good idea! https://github.com/svg/svgo/pull/1461
-            params: { overrides: { removeViewBox: false } },
           },
           { name: 'removeAttrs', params: { attrs: '(fill|stroke)' } },
         ],
--- test-app/tests/test.js
+++ test-app/tests/test.js
@@ -12,67 +12,75 @@ test('SVG as inline component', async ({ page }) => {
   await page.goto('/')
   const svg = page.locator('#inline-comp > svg')
   expect(await outerHtml(svg)).toMatchSnapshot('inline-comp.svg')
+  await expect(svg).toHaveScreenshot()
 })
 
 test('SVG as inline string', async ({ page }) => {
   await page.goto('/')
   const svg = page.locator('#inline-string > svg')
   expect(await outerHtml(svg)).toMatchSnapshot('inline-string.svg')
+  await expect(svg).toHaveScreenshot()
 })
 
-// FIXME: This test breaks when running in this test context (we get a data url)
-//        but it works fine when you look at the page in a browse when running
-//        the server
-test.skip('As URL in an image tag', async ({ page }) => {
+test('As URL in an image tag', async ({ page }) => {
   await page.goto('/')
   const img = page.locator('#image > img')
-  expect((await img.getAttribute('src')).includes('sample-logo')).toEqual(true)
-  expect(await img.getAttribute('alt')).toEqual('Sample logo')
+  await expect(img).toHaveAttribute(
+    'src',
+    expect.stringContaining('sample-logo')
+  )
+  await expect(img).toHaveAttribute('alt', 'Sample logo')
+  await expect(img).toHaveScreenshot()
 })
 
 test('With component props', async ({ page }) => {
   await page.goto('/')
   const svg = page.locator('#with-props > svg')
-  expect(await svg.getAttribute('width')).toEqual('200')
-  expect(await svg.getAttribute('aria-hidden')).toEqual('false')
+  await expect(svg).toHaveAttribute('width', '200')
+  await expect(svg).toHaveAttribute('aria-hidden', 'false')
+  await expect(svg).toHaveScreenshot()
 })
 
 test('From another directory', async ({ page }) => {
   await page.goto('/')
   const svg = page.locator('#from-another-dir > svg')
   expect(await outerHtml(svg)).toMatchSnapshot('from-another-dir.svg')
+  await expect(svg).toHaveScreenshot()
 })
 
 test('As data URL', async ({ page }) => {
   await page.goto('/')
   const img = page.locator('#dataurl > img')
   expect(await img.getAttribute('src')).toMatchSnapshot('base64imageurl.txt')
+  await expect(img).toHaveScreenshot()
 })
 
 test('As data URL with explicit base64', async ({ page }) => {
   await page.goto('/dataurl')
   const img = page.locator('#b64e > img')
   expect(await img.getAttribute('src')).toMatchSnapshot('base64explicit.txt')
+  await expect(img).toHaveScreenshot()
 })
 
 test('As data URL with explicit enc', async ({ page }) => {
   await page.goto('/dataurl')
   const img = page.locator('#enc > img')
   expect(await img.getAttribute('src')).toMatchSnapshot('enc.txt')
+  await expect(img).toHaveScreenshot()
 })
 
 test('As data URL with explicit unenc', async ({ page }) => {
   await page.goto('/dataurl')
   const img = page.locator('#unenc > img')
   expect(await img.getAttribute('src')).toMatchSnapshot('unenc.txt')
+  await expect(img).toHaveScreenshot()
 })
 
 test('Transform hook', async ({ page }) => {
   await page.goto('/hook')
   const svg = page.locator('svg > title')
-  const title = await svg.textContent()
 
-  expect(title).toEqual('Official SVG Logo')
+  await expect(svg).toHaveText('Official SVG Logo')
 })
 
 test('Optimized URL import', async ({ page }) => {
